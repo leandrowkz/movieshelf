971659505a43b21395bf12b182ab31f4206cf773
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-02-24 17:12:53 +0000

    feat: add services

---
+++ src/services/MovieAPI.ts
@@ -0,0 +1,29 @@
+import { Movie } from 'src/types/Movie'
+import { BaseAPI } from './BaseAPI'
+
+export class MovieAPI extends BaseAPI {
+  private accessToken: string
+
+  constructor() {
+    super(process.env.API_URL || '')
+    const accessToken = process.env.API_ACCESS_TOKEN || false
+
+    if (!accessToken) {
+      throw Error('Missing API access token.')
+    }
+
+    this.accessToken = accessToken
+  }
+
+  public async fetchMovie(id: number): Promise<Movie> {
+    const path = this.getPath(`/movie/${id}`)
+
+    return this.get<Movie>(path)
+  }
+
+  private getPath(path: string) {
+    const params = new URLSearchParams({ api_key: this.accessToken })
+
+    return `${path}${params.toString()}`
+  }
+}
