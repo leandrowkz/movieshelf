8315ad89e5f0a13ec544f7079a9a8ef7049ca881
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-03-08 19:50:24 +0000

    feat: add scroll restoration to page wrapper

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,6 +1,12 @@
 import React, { PropsWithChildren } from 'react'
 import styles from './styles.module.css'
+import { ScrollRestoration } from 'react-router-dom'
 
 export function Page({ children }: PropsWithChildren) {
-  return <section className={styles.page}>{children}</section>
+  return (
+    <section className={styles.page}>
+      <ScrollRestoration />
+      {children}
+    </section>
+  )
 }
