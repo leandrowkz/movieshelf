812f7c75010dfc3b247ed80ba3015508a71541f4
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-03-08 12:08:20 +0000

    chore: add linters + hooks

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  jsxSingleQuote: false,
+};
