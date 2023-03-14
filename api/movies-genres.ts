9f9cf110c27395833c11525cd5ab6f26b6142fd5
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-03-14 17:58:26 +0000

    refactor: fix filter params in proxy api

--- api/movies-genres.ts
+++ api/movies-genres.ts
@@ -8,7 +8,9 @@ export const config = {
 export default async (req: Request) => {
   const { searchParams } = new URL(req.url)
 
-  const { results } = await api.discover.movies(searchParams)
+  const filters = Object.fromEntries(searchParams.entries())
+
+  const { results } = await api.discover.movies(filters)
 
   return json(results)
 }
