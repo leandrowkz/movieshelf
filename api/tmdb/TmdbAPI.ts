75bd8f7d75999dca1cdb19cfb0aedc954f498713
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-03-17 08:07:39 +0000

    feat: add genres api to tmdb api wrapper

--- api/tmdb/TmdbAPI.ts
+++ api/tmdb/TmdbAPI.ts
@@ -1,14 +1,17 @@
 import { TmdbDiscoverAPI } from './TmdbDiscoverAPI'
+import { TmdbGenresAPI } from './TmdbGenresAPI'
 import { TmdbMoviesAPI } from './TmdbMoviesAPI'
 import { TmdbTrendingAPI } from './TmdbTrendingAPI'
 
 class TmdbAPI {
   public discover: TmdbDiscoverAPI
+  public genres: TmdbGenresAPI
   public movies: TmdbMoviesAPI
   public trending: TmdbTrendingAPI
 
   constructor() {
     this.discover = new TmdbDiscoverAPI()
+    this.genres = new TmdbGenresAPI()
     this.movies = new TmdbMoviesAPI()
     this.trending = new TmdbTrendingAPI()
   }
