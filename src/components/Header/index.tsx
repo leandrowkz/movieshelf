fc3fc12322c16f1813b1e21a6e8cfdf7e3ec68da
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-03-29 20:52:14 +0100

    feat: add header menu for movies

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Logo } from '../Logo'
 import styles from './styles.module.css'
 import { Container } from '../Container'
+import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
     <Container>
       <header className={styles.header}>
-        <Logo />
+        <Logo className={styles.logo} />
+        <div className={styles.menu}>
+          <Link to="/">Movies</Link>
+        </div>
       </header>
     </Container>
   )
