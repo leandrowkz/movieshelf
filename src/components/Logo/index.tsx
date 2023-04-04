4f6aa99cfdec67924c692bae77a0ff999f7062da
Leandro Mangini Antunes <leandro.antunes@mollie.com>
2023-04-04 20:34:32 +0100

    refactor: remove unused components

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentPropsWithoutRef } from 'react'
 import { Link } from 'react-router-dom'
-import ImgLogo from '../../assets/logo.svg'
 import styles from './styles.module.css'
 import classNames from 'classnames'
 import { Heading } from '../Heading'
