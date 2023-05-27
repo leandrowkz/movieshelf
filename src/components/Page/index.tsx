69e31a2f00e62e07d9ae2b69cff3dad5349c18f2
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-05-27 19:49:53 +0100

    refactor: fix ScrollRestoration

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentPropsWithoutRef } from 'react'
+import { ScrollRestoration } from 'react-router-dom'
 import { Footer } from '../Footer'
 import styles from './styles.module.css'
 import { Header } from '../Header'
@@ -12,6 +13,7 @@ export function Page({
 
   return (
     <section className={classes}>
+      <ScrollRestoration />
       <Header data-testid="header" className={styles.header} />
       <section data-testid="content" className={styles.content}>
         {children}
