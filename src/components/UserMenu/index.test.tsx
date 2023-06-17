9ddce3db221816e18f3044e64b41db9e4c87f6cc
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-06-17 21:09:21 +0100

    refactor: comment unused menus for now

--- src/components/UserMenu/index.test.tsx
+++ src/components/UserMenu/index.test.tsx
@@ -4,22 +4,8 @@ import { UserMenu } from '.'
 
 const { renderComponent, screen } = useTesting()
 
-test('Should render header properly', async () => {
+test('Should render properly', async () => {
   renderComponent(<UserMenu />)
 
-  expect(screen.getByTestId('logo')).toBeVisible()
-  expect(screen.getByTestId('menu')).toBeVisible()
-  expect(screen.getByTestId('sponsor')).toBeVisible()
-})
-
-test('Should render menu links properly', async () => {
-  renderComponent(<UserMenu />)
-
-  const menuMovies = screen.getByTestId('menu-movies') as HTMLLinkElement
-  const menuSponsorship = screen.getByTestId('sponsor') as HTMLLinkElement
-
-  expect(menuMovies.href).toEqual(`${window.location.href}movies`)
-  expect(menuMovies.target).toEqual('')
-  expect(menuSponsorship.href).toEqual('https://github.com/sponsors/leandrowkz')
-  expect(menuSponsorship.target).toEqual('_blank')
+  expect(screen.getByTestId('user-menu-sign-up')).toBeVisible()
 })
