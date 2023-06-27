6dc75cab419bcfc22a47dafe4aa3edeefddbc175
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-06-27 20:12:20 +0100

    feat: add pagination component

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,53 @@
+import React, { HTMLAttributes, ReactNode } from 'react'
+import styles from './styles.module.css'
+import { Button } from '../Button'
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  pages: number
+  current: number
+  onPageChange: (page: number) => void
+}
+
+export function Pagination({ pages, current, onPageChange }: Props) {
+  const content: ReactNode[] = []
+  const maximumPages = 10
+  const currentPageChunk = Math.ceil(current / maximumPages)
+  const startPage = currentPageChunk * maximumPages - maximumPages + 1
+  const endPage =
+    startPage + maximumPages > pages ? pages : startPage + maximumPages - 1
+
+  content.push(
+    <Button
+      key="pagination-button-prev"
+      variant="outlined"
+      disabled={current <= 1}
+      onClick={() => onPageChange(current - 1)}
+    >{`<`}</Button>
+  )
+
+  for (let i = startPage; i <= endPage; i++) {
+    const active = current === i
+
+    content.push(
+      <Button
+        key={`pagination-button-${i}`}
+        active={active}
+        variant="outlined"
+        onClick={() => onPageChange(i)}
+      >
+        {i}
+      </Button>
+    )
+  }
+
+  content.push(
+    <Button
+      key="pagination-button-next"
+      disabled={current >= pages}
+      variant="outlined"
+      onClick={() => onPageChange(current + 1)}
+    >{`>`}</Button>
+  )
+
+  return <section className={styles.pagination}>{content}</section>
+}
